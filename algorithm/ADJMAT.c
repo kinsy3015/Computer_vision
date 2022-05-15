1522cd929dc68a0ac3cc8937bb66f6d501226055
Sunghan Kim <kinsy3015@naver.com>
2022-05-15 14:42:52 +0900

    update

---
+++ algorithm/ADJMAT.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <alloc.h>
+#include "instack.c"
+#include "intqueue.c"
+
+#define MAX_VERTEX 100
+
+int G[MAX_VERTEX][MAX_VERTEX];
+int check[MAX_VERTEX];
+
+FILE *fp;
+
+int name2int(char c){
+
+
+
+}
+
+int int2name(int i){
+
+
+
+}
+
+void input_adjmatrix(int a[][MAX_VERTEX], int *V, int *E){
+
+
+
+
+}
+
+void print_adjmatrix(int a[][MAX_VERTEX], int V){
+
+
+
+
+}
+
+void visit(int vertex){
+
+
+
+}
+
+void DFS_recur(int a[][MAX_VERTEX], int V, int i){
+
+
+
+
+}
+
+void DFS_adjmatrix(int a[][MAX_VERTEX], int V){
+
+
+
+}
+
+void nrDFS_adjmatrix(int a[][MAX_VERTEX], int V){
+
+
+
+}
+
+
+void BFS_adjmatrix(int
